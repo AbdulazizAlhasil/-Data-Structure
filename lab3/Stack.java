8bc775cc3458920584151ccb25833f2657e7a87f
AbdulazizAlhasil <73362516+AbdulazizAlhasil@users.noreply.github.com>
2022-02-06 21:41:36 +0300

    Create Stack.java

---
+++ lab3/Stack.java
@@ -0,0 +1,9 @@
+package Lab3;
+
+public interface Stack <A> {
+    public boolean isEmpty ();
+    public int size ();
+    public void push (A element);
+    public A pop ();
+    public A top ();
+}
